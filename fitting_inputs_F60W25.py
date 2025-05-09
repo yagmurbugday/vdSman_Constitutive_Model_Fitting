3929af971c06fc2380735cc0794a00af84aaf024
yagmurbugday <z.y.bugday@gmail.com>
2025-05-09 15:01:51 +0200

    working fine

--- fitting_inputs_F60W25.py
+++ fitting_inputs_F60W25.py
@@ -87,7 +87,7 @@ target_strain =120 # to extract the stress at
 # time  = [ freq0_StrainLin , freq1_StrainLin, freq2_StrainLin, ... , freqN_StrainLin, ]
 #for instance: data_exp_frswp_waveform_avg[0][0][1] has the stress data from all the frequencies sequentially. 
 strain_lin = 0.01
-freqvals_frfit= np.logspace(-1,1,9)
+freqvals_frfit= np.logspace(-1,1,10)
          
 
 #There are also datasets from strain sweeps 
@@ -121,7 +121,7 @@ ntimesteps_cycle = nelements_cycle -1
 
 #Option 1: Read a previous run case
 run_HB = 'no'
-readHB = 'HBfit_2025_05_08_14_54' #read this file if you do not run a new case
+readHB = 'HBfit_2025_05_09_14_44' #read this file if you do not run a new case
 
 #Option2: Run the HB fitting from scratch
 #run_HB = 'yes'
@@ -150,7 +150,7 @@ error_HBext = [0,0,0] #sigma_y, dot_gamma_cr, n
 
 #Option 2:
 runSAOS = 'no'
-readSAOS = 'SAOSfit_2025_05_08_14_54'
+readSAOS = 'SAOSfit_2025_05_09_14_44'
 
 #These inputs are used while performing the fitting
 #The initial guess for the SAOS fitting
@@ -179,7 +179,7 @@ init_guess_lin =  [modulus[0], t_rel[0]]
 
 #Option 2:
 runLAOS = 'no'
-readLAOS = 'LAOSfit_all_2025_05_08_14_54'
+readLAOS = 'LAOSfit_all_2025_05_09_14_44'
 
 #The initial guess for the LAOS fitting
 tau_y_log=[0.5] 
